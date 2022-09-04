202c4221769977a1e0a06f5e95b5a6b9493af5fc
thanh tam <1951052176tam@ou.edu.vn>
2022-09-04 11:35:58 +0700

    Updates

---
+++ precache-manifest.8cc7fe2521c0151e9dfbb5dcec0b1e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c17531650bb1cc062baff10ceeb9b1d",
+    "url": "/ecourse/index.html"
+  },
+  {
+    "revision": "1d0bfb4c07cec3c00ae6",
+    "url": "/ecourse/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "9d5471902077d0a2c3b4",
+    "url": "/ecourse/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "1d0bfb4c07cec3c00ae6",
+    "url": "/ecourse/static/js/2.768bee3b.chunk.js"
+  },
+  {
+    "revision": "9d5471902077d0a2c3b4",
+    "url": "/ecourse/static/js/main.d8e26d86.chunk.js"
+  },
+  {
+    "revision": "0f46b6483170a14a806b",
+    "url": "/ecourse/static/js/runtime~main.4295fb29.js"
+  }
+]);
